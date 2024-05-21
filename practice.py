6e5de91f14399ba909f2c4d030549268fda58d12
gorla <alessandra.gorla@gmail.com>
2024-05-21 15:04:30 +0200

    object-oriented version - ex 2

--- practice.py
+++ practice.py
@@ -1,11 +1,19 @@
-def lists_division(x, y):
-    sum_x = sum(x)
-    sum_y = sum(y)
+class ListCalculator:
+    def __init__(self, x_list, y_list):
+        self.x = x_list
+        self.y = y_list
     
-    return sum_y / sum_x
+    def lists_division(self):
+              
+        sum_x = sum(self.x)
+        sum_y = sum(self.y)
+        
+        return sum_y / sum_x
 
 # Example usage:
 x_values = [10, 20, 30]
 y_values = [5, 10, 15]
-result = lists_division(x_values, y_values)
+
+calculator = ListCalculator(x_values, y_values)
+result = calculator.lists_division()
 print("Result:", result)
